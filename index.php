969404e1f4df87a9a4ae87ace0ee7c5e4b606487
yordy0095 <yordy0095@hotmail.com>
2017-05-09 20:25:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,36 @@
 	<link rel="stylesheet" href="style.css" />
 </head>
 <body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Parcial de Cloud Yordy"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
+	<div class="marco">
+		<img src="images/Cabecera.JPG" style="width: 100%; height: auto;" />
+		<th style="width: 400px; align: center;">For data scientist</th>
+		<th style="width: 400px; align: center;">For IT professionals</th>
+		<?php
+		$servername = "us-cdbr-iron-east-03.cleardb.net";
+		$username = "b74ba3320e82ec";
+		$password = "2d194843";
+		$dbname = "ad_bb6ddb2f34daf52";
+		
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		if ($conn->connect_error) {
+		  die("Connection failed: " . $conn->connect_error);
+		}
+		
+		$sql = "SELECT * from servicios";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) { ?>
+		<table>
+		  <?php while($row = $result->fetch_assoc()) { ?>
+		  	<tr style="width: 50%; float: left; height: 114px;">
+		      <td><img src="<?php echo $row['imagen_servicio'] ?>" /></td>
+		      <td><b><?php echo $row['nombre_servicio']?></b> <br /> <?php echo $row['descripcion_servicio']?></td>
+		    </tr>
+		  <?php } ?>
+		</table>
+		<?php }  
+		$conn->close();
+		?> 
+		<img src="images/Pie.JPG" style="width: 100%; height: auto;" />
+	</div>
 </body>
 </html>
